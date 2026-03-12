0f8680e732a89981ea3e1812b382f8ce010d9af7
pankaj-mahor <mahorpankaj12@gmail.com>
2026-03-13 01:56:04 +0530

    interview apis get all and get by ID

--- Backend/src/controller/interview.controller.js
+++ Backend/src/controller/interview.controller.js
@@ -1,6 +1,7 @@
 const interviewReportModel = require("../models/interviewReport.model");
 const pdfParse = require("pdf-parse");
 const { generateInterviewReport } = require("../services/ai.service");
+const { get } = require("mongoose");
 /**
  * @route POST /api/interview/generate-interview-report
  * @description Generate an interview report for a candidate on basis of job description and resume
@@ -51,6 +52,67 @@ const generateInterviewReportController = async (req, res) => {
   });
 };
 
+/**
+ *
+ * @route GET /api/interview/:interviewId
+ * @description Get an interview report for a candidate
+ * @access Private
+ * @returns {Object} 200 - Interview report fetched successfully
+ * @returns {Object} 400 - Bad request
+ * @returns {Object} 500 - Internal server error
+ *
+ * @param {*} req
+ * @param {*} res
+ */
+
+const getInterviewReportIdController = async (req, res) => {
+  const { interviewId } = req.params;
+  const interviewReport = await interviewReportModel.findOne({
+    _id: interviewId,
+    user: req.user.id,
+  });
+  if (!interviewReport) {
+    return res
+      .status(400)
+      .json({ message: "Interview report not found", success: false });
+  }
+  res.status(200).json({
+    message: "Interview report fetched successfully",
+    interviewReport,
+    success: true,
+  });
+};
+
+/**
+ * @route GET /api/interview/all
+ * @description Get all interview reports for a candidate
+ * @access Private
+ * @returns {Object} 200 - Interview reports fetched successfully
+ * @returns {Object} 400 - Bad request
+ * @returns {Object} 500 - Internal server error
+ *
+ * @param {*} req
+ * @param {*} res
+ */
+
+const getAllInterviewReportsController = async (req, res) => {
+  const interviewReports = await interviewReportModel.find({
+    user: req.user.id,
+  });
+  if (!interviewReports) {
+    return res
+      .status(400)
+      .json({ message: "No interview reports found", success: false });
+  }
+  res.status(200).json({
+    message: "Interview reports fetched successfully",
+    interviewReports,
+    success: true,
+  });
+};
+
 module.exports = {
   generateInterviewReportController,
+  getAllInterviewReportsController,
+  getInterviewReportIdController,
 };
--- Backend/src/routes/interview.route.js
+++ Backend/src/routes/interview.route.js
@@ -8,16 +8,36 @@ const fileMiddleware = require("../middlewares/file.middleware");
  * @route POST /api/interview/generate-interview-report
  * @description Generate an interview report for a candidate on basis of job description and resume
  * @access Private
- * @returns {Object} 200 - Interview report generated successfully
- * @returns {Object} 400 - Bad request
- * @returns {Object} 500 - Internal server error
- *
  */
 interviewRouter.post(
   "/generate-interview-report",
   authMiddleware.authenticateToken,
   fileMiddleware.single("resume"),
   interviewController.generateInterviewReportController,
+);
+
+/**
+ *
+ * @route GET /api/interview/:interviewId
+ * @description Get an interview report for a candidate
+ * @access Private
+ */
+interviewRouter.get(
+  "/:interviewId",
+  authMiddleware.authenticateToken,
+  interviewController.getInterviewReportIdController,
+);
+
+/**
+ *
+ * @route GET /api/interview/all
+ * @description Get all interview reports for a candidate
+ * @access Private
+ */
+interviewRouter.get(
+  "/all",
+  authMiddleware.authenticateToken,
+  interviewController.getAllInterviewReportsController,
 );
 
 module.exports = interviewRouter;
